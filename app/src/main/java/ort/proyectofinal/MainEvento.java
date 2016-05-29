1b4860c55cef0db8856c617ff663cfaddf1fd6c5
maschuster <m.a.schuster98@gmail.com>
2016-05-29 14:01:38 -0300

    Ahora hay una lista de objetos y desde una barra que se extiende del FAB se puede agregar otro objeto.
    Todavía hay que arreglar que no se ve bien el ListView donde están todos los objetos. (ScrollView)

--- app/src/main/java/ort/proyectofinal/MainEvento.java
+++ app/src/main/java/ort/proyectofinal/MainEvento.java
@@ -1,19 +1,27 @@
 package ort.proyectofinal;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,7 +29,15 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
@@ -33,7 +49,7 @@ import java.util.Date;
 import java.util.List;
 
 
-public class MainEvento extends AppCompatActivity implements OnMapReadyCallback {
+public class MainEvento extends AppCompatActivity implements OnMapReadyCallback,View.OnClickListener {
 
     GoogleMap map;
     Evento e;
@@ -41,6 +57,12 @@ public class MainEvento extends AppCompatActivity implements OnMapReadyCallback 
     TextView TVdescripcion;
     CalendarView calendarioVW;
     DatePicker Datepicker;
+    private FABToolbarLayout layout;
+    private View one, two, three, four;
+    private ListView list;
+    private View fab;
+    ArrayList<Objeto> objetos;
+    String url = "http://192.168.0.7/refreshobjetos.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,19 +72,19 @@ public class MainEvento extends AppCompatActivity implements OnMapReadyCallback 
         TextView TVdescripcion = (TextView) findViewById(R.id.descripcion);
         CalendarView CalendarioVW = (CalendarView) findViewById(R.id.calendarView);
         DatePicker Datepicker = (DatePicker) findViewById(R.id.datePicker);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+        Bundle extras = getIntent().getExtras();
+        e = (Evento) extras.getSerializable("evento");
+        toolbar.setTitle(e.getNombre());
+        setSupportActionBar(toolbar);
+        TVdescripcion.setText(e.getDescripcion());
+        String dirStr = e.getLugar();
+        if (!dirStr.isEmpty()) {
+            new GeolocalizacionTask().execute(dirStr);
+        }
+        /*SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
         Date date = null;
         try {
             date = (Date)formatter.parse(e.getFecha());
@@ -70,12 +92,140 @@ public class MainEvento extends AppCompatActivity implements OnMapReadyCallback 
             e1.printStackTrace();
         }
         long mills = date.getTime();
-        calendarioVW.setDate(mills);
-        Bundle extras = getIntent().getExtras();
-        e = (Evento) extras.getSerializable("evento");
-        toolbar.setTitle(e.getNombre());
-        setSupportActionBar(toolbar);
-        TVdescripcion.setText(e.getDescripcion());
+        calendarioVW.setDate(mills);*/
+
+        objetos = new ArrayList<>();
+        new ObjetosTask().execute(url);
+        layout = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        list = (ListView) findViewById(R.id.list);
+        fab = findViewById(R.id.fabtoolbar_fab);
+        one.setOnClickListener(this);
+        two.setOnClickListener(this);
+        three.setOnClickListener(this);
+        four.setOnClickListener(this);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.show();
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        layout.hide();
+    }
+    @Override
+    public void onClick(View v) {
+        showChangeLangDialog();
+    }
+    public void showChangeLangDialog() {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.agregar_objeto, null);
+        dialogBuilder.setView(dialogView);
+
+        dialogBuilder.setTitle("Agregar Nuevo Objeto");
+        dialogBuilder.setMessage("Ingrese los datos");
+        dialogBuilder.setPositiveButton("Listo", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                EditText edt = (EditText) dialogView.findViewById(R.id.nombre);
+                EditText edt2 = (EditText) dialogView.findViewById(R.id.precio);
+                final String nombre = edt.getText().toString();
+                final int precio = Integer.parseInt(edt2.getText().toString());
+                AgregarObjetoSQL(nombre,precio);
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+    }
+
+
+
+    public void AgregarObjetoSQL (String nombre, int precio) {
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        try {
+            OkHttpClient client = new OkHttpClient();
+            String url ="http://192.168.0.7/agregarobjeto.php";
+            JSONObject json = new JSONObject();
+            json.put("nombre", nombre);
+            json.put("precio", precio);
+
+            RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json.toString());
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            Log.d("Response", response.body().string());
+        } catch (IOException | JSONException e) {
+            Log.d("Error", e.getMessage());
+        }
+        new ObjetosTask().execute(url);
+
+    }
+    private class ObjetosTask extends AsyncTask<String, Void, ArrayList<Objeto>> {
+        private OkHttpClient client = new OkHttpClient();
+
+        @Override
+        protected void onPostExecute(ArrayList<Objeto> objetosResult) {
+            super.onPostExecute(objetosResult);
+            if (!objetosResult.isEmpty()) {
+                objetos = objetosResult;
+                ObjetoAdapter adapter = new ObjetoAdapter(getApplicationContext(),objetosResult);
+                list.setAdapter(adapter);
+            }
+        }
+
+        @Override
+        protected ArrayList<Objeto> doInBackground(String... params) {
+            String url = params[0];
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                return parsearResultado(response.body().string());
+            } catch (IOException | JSONException e) {
+                Log.d("Error", e.getMessage());
+                return new ArrayList<Objeto>();
+            }
+        }
+
+        ArrayList<Objeto> parsearResultado(String JSONstr) throws JSONException {
+            ArrayList<Objeto> objetos = new ArrayList<>();
+            JSONArray jsonObjetos = new JSONArray(JSONstr);
+            for (int i = 0; i < jsonObjetos.length(); i++) {
+                JSONObject jsonResultado = jsonObjetos.getJSONObject(i);
+                int idObjeto = jsonResultado.getInt("idObjeto");
+                String nombre = jsonResultado.getString("nombre");
+                int precio = jsonResultado.getInt("precio");
+
+                Objeto o = new Objeto(nombre,precio,idObjeto);
+                objetos.add(o);
+            }
+            return objetos;
+        }
+
     }
 
 
@@ -87,21 +237,7 @@ public class MainEvento extends AppCompatActivity implements OnMapReadyCallback 
         map.getUiSettings().setZoomControlsEnabled(true);
 
     }
-
-    public void consultarDireccion(View v) {
-        String dirStr = e.getLugar();
-        if (!dirStr.isEmpty()) {
-            new GeolocalizacionTask().execute(dirStr);
-        }
-    }
-
-    // Utiliza la clase android.location.Geocoder
-    // Parametros
-    // String - la direccion a buscar que recibe doInBackground
-    // Void -  Progreso (no se usa)
-    // List<Address> - lo que devuelve doInBackground
     private class GeolocalizacionTask extends AsyncTask<String, Void,List<Address>> {
-
         @Override
         protected void onPostExecute(List<Address> direcciones) {
             super.onPostExecute(direcciones);
