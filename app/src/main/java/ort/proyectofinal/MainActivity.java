d8f4fe9aa73d2870ee0b6c1edc9261fe182fb84b
41471933 <m.a.schuster98@gmail.com>
2016-07-15 10:44:44 -0300

    Agregando SDK

--- app/src/main/java/ort/proyectofinal/MainActivity.java
+++ app/src/main/java/ort/proyectofinal/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
     }
 
