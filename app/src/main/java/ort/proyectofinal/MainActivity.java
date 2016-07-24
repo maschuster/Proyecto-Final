5be4a3e86bcf0fb9d09b55723b65f2d57371f37f
maschuster <m.a.schuster98@gmail.com>
2016-07-24 20:37:12 -0300

    Login con facebook funcionando y trayendo mediante JSON email,id de facebook, y nombre.

--- app/src/main/java/ort/proyectofinal/MainActivity.java
+++ app/src/main/java/ort/proyectofinal/MainActivity.java
@@ -10,37 +10,89 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-public class MainActivity extends FragmentActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class MainActivity extends FragmentActivity {
+    CallbackManager callbackManager;
+    LoginButton loginButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(this.getApplicationContext());
+        setContentView(R.layout.activity_main);
 
-        LoginFragment fragment = new LoginFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction()
-                .replace(R.id.contenido,fragment)
-                .commit();
+        callbackManager = CallbackManager.Factory.create();
 
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setReadPermissions("public_profile");
+        loginButton.setReadPermissions("email");
 
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(MainActivity.this, "User ID: " + loginResult.getAccessToken().getUserId() + "\n" +"Auth Token: "+ loginResult.getAccessToken().getToken(), Toast.LENGTH_SHORT).show();
+                        System.out.println("Success");
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject json, GraphResponse response) {
+                                        if (response != null) {
+                                            try {
+                                                String jsonresult = String.valueOf(json);
+                                                System.out.println("JSON Result"+jsonresult);
 
+                                                String mFbid = json.getString("id");
+                                                String mFullname = json.getString("name");
+                                                String email = json.getString("email");
+
+                                                // Meter en un objeto USUARIO para mandar después en una función a la base de datos.
+                                                //Redirigir a ListarEventos y que filtre eventos por usuario.
+
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,email");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
     }
 
-
-    public void btnListarEventos(View view) {
-        Intent intent = new Intent(this, ListarEventos.class);
-        startActivity(intent);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
